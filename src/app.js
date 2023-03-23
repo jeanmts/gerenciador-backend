5e85dfbf40bb063a0b314d40e8809a298cd97474
jean carlos <matos.sd52@gmail.com>
2023-03-23 14:15:20 -0300

    config cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const verifyAuthentication = require("./middleware/verifyAuthentication");
 const app = express();
+const cors = require("cors");
 const routes = require("./router/routes");
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(port);
